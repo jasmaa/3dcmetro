c1c2123ddfadce8a33c497fbe0068c4013fe4556
jasmaa <jasonmaa3955@gmail.com>
2026-06-02 00:30:16 -0400

    chore: Upgrade API to vitest 4.1.0

--- 3dcmetro-api/test/index.spec.ts
+++ 3dcmetro-api/test/index.spec.ts
@@ -1,7 +1,10 @@
 // test/index.spec.ts
-import { env, createExecutionContext, waitOnExecutionContext, fetchMock } from 'cloudflare:test';
-import { describe, it, expect, beforeAll } from 'vitest';
+import { env } from 'cloudflare:workers';
+import { createExecutionContext, waitOnExecutionContext } from 'cloudflare:test';
+import { http, HttpResponse } from "msw";
+import { describe, it, expect, beforeAll, afterEach, afterAll, vi } from 'vitest';
 import worker from '../src/index';
+import { server } from './server';
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
@@ -10,17 +13,31 @@ const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 describe('API worker', () => {
 
 	beforeAll(() => {
-		fetchMock.activate();
-		fetchMock.disableNetConnect();
+		vi.useFakeTimers();
+		vi.setSystemTime(1663516715115);
+
+		server.listen({
+			onUnhandledRequest: "error",
+		});
 	});
 
+	afterEach(() => server.resetHandlers());
+	afterAll(() => server.close());
+
 	it('responds with train locations when getting /trainlocations', async () => {
-		const origin = fetchMock.get('https://gisservices.wmata.com');
-		origin
-			.intercept({ path: `/gisservices/rest/services/Public/TRAIN_LOC_WMS_PUB/MapServer/0/query?f=json&where=TRACKLINE%3C%3E%20%27Non-revenue%27%20and%20TRACKLINE%20is%20not%20null&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*`, method: 'GET' })
-			.reply(200, {
-				displayFieldName: "TRACKNAME",
-			});
+		server.use(
+			http.get(
+				'https://gisservices.wmata.com/gisservices/rest/services/Public/TRAIN_LOC_WMS_PUB/MapServer/0/query?f=json&where=TRACKLINE%3C%3E%20%27Non-revenue%27%20and%20TRACKLINE%20is%20not%20null&returnGeometry=true&spatialRel=esriSpatialRelIntersects&outFields=*',
+				() => {
+					return HttpResponse.json(
+						{
+							displayFieldName: "TRACKNAME",
+						},
+						{ status: 200 },
+					);
+				},
+			),
+		);
 
 		const request = new IncomingRequest('http://example.com/trainlocations');
 
